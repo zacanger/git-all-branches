2e8a59219d917dde16494d2de0de85b519f5ae37
zacanger <zac@zacanger.com>
2020-04-28 18:46:58 -0600

    chore: remove unused dep

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { execSync } from 'child_process'
 
 const remoteName = process.argv[2] || 'origin'
 
+/*
 const reset = '\u001b[0m'
 const blue = '\u001b[0;34m'
 const cyan = '\u001b[0;36m'
 const green = '\u001b[0;32m'
 const yellow = '\u001b[0;33m'
 const red = '\u001b[0;31m'
-
-module.exports = [firstRow, secondRow].join('') + reset
+*/
 
 const cleanUpLists = (s = '') =>
   s
