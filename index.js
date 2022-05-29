73de660004e99004300d25d3d4b5d879c4a3e48b
zacanger <zac@zacanger.com>
2022-05-29 14:18:28 -0600

    chore: cleanup, switch to standard

--- index.js
+++ index.js
@@ -1,5 +1,78 @@
 #!/usr/bin/env node
 
-if (!module.parent) {
-  require('./lib')
+const { execSync } = require('child_process')
+
+const remoteName = process.argv[2] || 'origin'
+
+const reset = '\u001b[0m'
+const green = '\u001b[0;32m'
+const yellow = '\u001b[0;33m'
+const red = '\u001b[0;31m'
+
+const isInGitRepo = () => {
+  try {
+    return (
+      execSync('git rev-parse --is-inside-work-tree').toString().trim() ===
+      'true'
+    )
+  } catch {
+    return false
+  }
+}
+
+if (!isInGitRepo()) {
+  process.exit(1)
+} else {
+  const cleanUpLists = (s = '') =>
+    s
+      .split('\n')
+      .map((a) => a.trim())
+      .map((a) => a.replace(`${remoteName}/`, ''))
+      .filter((a) => !a.includes('->'))
+
+  const currentBranch = execSync('git rev-parse --abbrev-ref HEAD')
+    .toString()
+    .trim()
+
+  const remoteBranches = cleanUpLists(
+    execSync('git branch -r').toString().trim()
+  )
+
+  const localBranches = cleanUpLists(
+    execSync('git branch').toString().trim().replace('* ', '')
+  )
+
+  const uniq = (xs) => xs.filter((v, i, s) => s.indexOf(v) === i)
+
+  const allBranches = uniq(
+    [...remoteBranches, ...localBranches].sort((a, b) => a.localeCompare(b))
+  )
+
+  const onlyRemote = allBranches.filter((a) => !localBranches.includes(a))
+
+  const onlyLocal = allBranches.filter((a) => !remoteBranches.includes(a))
+
+  const onBoth = allBranches.filter(
+    (a) => remoteBranches.includes(a) && localBranches.includes(a)
+  )
+
+  const annotatedBranches = allBranches.map((a) => {
+    let branchString = `  ${a}`
+    if (a === currentBranch) {
+      branchString = `* ${a}`
+    }
+    if (onlyRemote.includes(a)) {
+      branchString = `${yellow}${branchString} (remote)${reset}`
+    }
+    if (onlyLocal.includes(a)) {
+      branchString = `${red}${branchString} (local)${reset}`
+    }
+    if (onBoth.includes(a)) {
+      branchString = `${green}${branchString} (both)${reset}`
+    }
+
+    return branchString
+  })
+
+  console.log(annotatedBranches.join('\n'))
 }
